c4d6424328295f881c4bf6da37c3cea5fcdb2fde
wadsdaniel <danielwaduka@gmail.com>
2023-08-02 08:23:47 +0300

    improvements

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-import express from 'express';
+import express from "express";
+import { SuperfaceClient } from "@superfaceai/one-sdk";
 const app = express();
-app.set('trust proxy', true)
+app.set("trust proxy", true);
 
-app.get('/', async (req, res) => {
-    const ip = req.ip
-    res.send(ip);
-})
+const sdk = new SuperfaceClient();
+
+async function run(ip) {
+  // Load the profile
+  const profile = await sdk.getProfile("address/ip-geolocation@1.0.1");
+
+  // Use the profile
+  const result = await profile.getUseCase("IpGeolocation").perform(
+    {
+      ipAddress: ip
+    },
+    {
+      provider: "ipdata",
+      security: {
+        apikey: {
+          apikey: "587a023fd9e8a83cb5d5ffb29e64d10964e7fe28df63f8a7449fabcc"
+        }
+      }
+    }
+  );
+
+  // Handle the result
+  try {
+    const data = result.unwrap();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.get("/", async (req, res) => {
+  res.send(await run(req.ip));
+});
 
 app.listen(3000, () => {
-    console.log("Server listening on 3000")
-})
+  console.log("SERVER RUNNIHG AT PORT 3000");
+});
